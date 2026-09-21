9fd5c24d56eac5a75c7ba9aad73a2ad528a7de09
Zina <ghostshadow0316@proton.me>
2026-09-21 22:12:54 +0800

    add folour module on web

--- web/bomb/lib/components.js
+++ web/bomb/lib/components.js
@@ -5,14 +5,33 @@ link.href = "./lib/components.css";
 document.head.appendChild(link);
 
 $("led").each( function () {
-    this.setColour = function (colour) {
-        $(this).attr("colour", colour);
-        $(this).css("backgroundColor", colour);
+    this.setON = function () {
+        $(this).attr("state", 1);
+        $(this).css("backgroundColor", $(this).attr("colour"));
+        $(this).css("border", "");
+    }
+    
+    this.setOFF = function () {
+        $(this).attr("state", 0);
+        $(this).css("backgroundColor", "transparent");
+        $(this).css("border", "2px solid black");
     }
 
-    this.toggleOnOff = function () {
-        state = !$(this).attr("state");
-        $(this).attr("state", state)
-        $(this).css("backgroundColor", state ? "white" : $(this).attr("colour") = colour);
+    this.setState = function (state) {
+        $(this).attr("state", state);
+        if (state == 1) this.setON();
+        if (state == 0) this.setOFF();
     }
+
+    this.setColour = function (colour) {
+        $(this).attr("colour", colour);
+        if ($(this).attr("state") == "1") $(this).css("backgroundColor", colour);
+    }
+
+    this.setColour($(this).attr("colour"));
+
+    state = $(this).attr("state");
+    if (state == undefined) this.setState(1);
+    if (state == 1) this.setON();
+    if (state == 0) this.setOFF();
 } );
---
+++ web/bomb/script/folour.js
@@ -0,0 +1,92 @@
+
+const _R = "red";
+const _G = "green";
+const _B = "blue";
+const _W = "white";
+
+const memFolourPatterns = [
+    [
+        [_G, _R, _B, _B, _W],
+        [_R, _B, _R, _B, _G, _W],
+        [_R, _R, _B, _G, _G, _W],
+        [_B, _B, _R, _G, _W],
+    ],
+    [
+        [_G, _R, _G, _B, _W],
+        [_R, _B, _G, _W],
+        [_G, _R, _B, _R, _G, _W],
+        [_G, _B, _R, _W],
+    ],
+    [
+        [_R, _G, _B, _G, _R, _W],
+        [_B, _G, _R, _G, _G, _W],
+        [_B, _R, _G, _B, _W],
+        [_G, _R, _R, _B, _W],
+    ],
+    [
+        [_R, _G, _B, _B, _W],
+        [_R, _G, _B, _G, _B, _W],
+        [_B, _R, _G, _R, _W],
+        [_G, _G, _R, _G, _R, _W],
+    ],
+];
+
+const memFolourAnswers = [
+    [_R, _R, _B, _G],
+    [_G, _R, _B, _G],
+    [_R, _B, _R, _B],
+    [_B, _G, _G, _R],
+];
+
+let YB = Math.floor(Math.random() * 4);
+$("#ledY")[0].setState(YB >> 1);
+$("#ledB")[0].setState(YB % 2);
+
+let patterns = memFolourPatterns[YB];
+let answer   = memFolourAnswers[YB];
+let user    = [0, 0, 0, 0];
+let toggles = [0, 0, 0, 0];
+let fixed   = 0;
+
+$("main led").each(function (ledIdx) {
+    this.patternIdx = 0;
+    this.interval = null;
+
+    this.colourChange = () => {
+        pattern = patterns[ledIdx][this.patternIdx];
+        user[ledIdx] = pattern;
+        this.setColour(pattern);
+
+        this.patternIdx = (this.patternIdx + 1) % patterns[ledIdx].length;
+    }
+
+    toggles[ledIdx] = () => {
+        if (this.interval !== null) {
+            fixed += 1;
+            clearInterval(this.interval);
+            this.interval = null;
+            return;
+        }
+        fixed -= 1;
+        this.interval = setInterval(this.colourChange, 250);
+    }
+
+    toggles[ledIdx]();
+    fixed = 0;
+});
+
+function check () {
+    return (JSON.stringify(fixed) == 4 && JSON.stringify(user)  == JSON.stringify(answer))
+}
+
+$("main btn").each(function (idx) {
+    $(this).on("click", () => {
+        toggles[idx]();
+        fixed[idx] = !fixed[idx];
+
+        if (check()) {
+            alert("you win!");
+            location.href = location.href;
+        }
+    });
+});
